2a397b38aa13cdcf85a42bac159339741741bbeb
Dominik S. <152535653+Domiko7@users.noreply.github.com>
2025-12-28 13:44:40 +0100

    Refactor ping command execution logic

--- src/commands/utils/ping.js
+++ src/commands/utils/ping.js
@@ -6,16 +6,16 @@ module.exports = {
     .setName("ping")
     .setDescription("Used to check if the bot is online, replies with pong."),
   async execute(interaction) {
-  const embed = new EmbedBuilder()
-    .setColor(0xFF0000)
-    .setTitle("Pong - 🏓")
-    .setTimestamp()
-    .setFooter({ text: "Made by Domiko" });
-
-    const start = Date.now();
-    const message = await interaction.reply({ embeds: [embed], withResponse: true });
-    const delay = Date.now() - start;
-    embed.setDescription(`Delay: ${delay}ms`)
-    await interaction.editReply({ embeds: [embed] });
-  },
+    const embed = new EmbedBuilder()
+      .setColor(0xFF0000)
+      .setTitle("Pong - 🏓")
+      .setTimestamp()
+      .setFooter({ text: "Made by Domiko" });
+  
+      const start = Date.now();
+      const message = await interaction.reply({ embeds: [embed], withResponse: true });
+      const delay = Date.now() - start;
+      embed.setDescription(`Delay: ${delay}ms`)
+      await interaction.editReply({ embeds: [embed] });
+    },
 };
